73132744fb7a06b10e4c26af367764ea60ccad92
nichtmetall <hofmannanton@outlook.de>
2022-11-14 07:35:58 +0100

    changes

--- rental_project/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppProps } from 'next/app'
-import { initializeIcons } from "@fluentui/react/lib/Icons";
-initializeIcons();
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
