9a852f6a4764eed0cbf140259c8b7aecfc5ac6f8
Richard Peters <rpe@xima.de>
2022-11-15 10:31:54 +0100

    add sample firebase db get data function

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import ProtectedRoute from "../components/ProtectedRoute";
+import { getDatabase, ref, get } from "firebase/database";
 
 const DashboardPage = () => {
+    const dbRef = ref(getDatabase());
+    const [geb, setGeb] = useState("");
+
+    get(dbRef).then((snapshot) => {
+    if (snapshot.exists()) {
+        setGeb(snapshot.val().test);
+    } else {
+        console.log("No data available");
+    }
+    }).catch((error) => {
+    console.error(error);
+    });
+
     return (
         <ProtectedRoute>
             <div>
-                <h2>Perfekt, deine e-mail:</h2>
+                <h2>Perfekt, deine e-mail ist:{geb}</h2>
             </div>
         </ProtectedRoute>
     );
