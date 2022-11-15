6678301da4c3b95e4995445ec757a5f27e2b3c08
nichtmetall <hofmannanton@outlook.de>
2022-11-15 08:09:40 +0100

    Designed Login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,24 +1,18 @@
 export default function Login() {
     return (
-        <div className="justify-center">
-            <div className="max-w-sm ">
+        <div className="grid justify-items-center h-screen items-center">
+            <div className="space-y-4">
                 <div>
-                    <label htmlFor="price" className="block text-sm font-medium text-gray-700">
-                        Email
-                    </label>
                     <div className="relative mt-1 rounded-md shadow-sm">
                         <input
                             type="text"
                             id="email"
                             className="block w-full rounded-md border-gray-300 pl-2 pr-12 focus:border-green-600 focus:ring-green-600 sm:text-sm transition"
-                            placeholder="email@example.com"
+                            placeholder="Email"
                         />
                     </div>
                 </div>
                 <div>
-                    <label htmlFor="price" className="block text-sm font-medium text-gray-700">
-                        Passwort
-                    </label>
                     <div className="relative mt-1 rounded-md shadow-sm">
                         <input
                             type="password"
@@ -27,6 +21,11 @@ export default function Login() {
                             placeholder="Passwort"
                         />
                     </div>
+                </div>
+                <div>
+                    <button id="btnLogin" className='w-full text-white px-4 py-2 text-base font-medium rounded-lg bg-green-600 hover:bg-green-500 transition'>
+                        Anmelden &rarr;
+                    </button>
                 </div>
             </div>
         </div>
