473a610032b9ce02ccfd898d8b996f7f1888720d
Nichtmetall <hofmannanton@outlook.de>
2022-12-13 08:45:13 +0100

    Style changes

--- pages/bookings/new.tsx
+++ pages/bookings/new.tsx
@@ -15,9 +15,9 @@ import { ref, set } from "firebase/database";
 import CheckboxGroup from "../../components/bookings/checkboxGroup";
 import RadioButtons from "../../components/bookings/radioButtons";
 
-type Obj = {[key: string] : string}
+type Obj = { [key: string]: string }
 const booking: Obj = {}
-export const setBookingValue = (value:any, prop:any) => {
+export const setBookingValue = (value: any, prop: any) => {
     booking[prop] = value
     console.log(booking)
 }
@@ -44,7 +44,7 @@ export default function NewBooking() {
         }
     }
 
-    function validateWorkPlaceType(){
+    function validateWorkPlaceType() {
         //check if 
         /*if(data.workplacetype == "Doppelarbeitsplatz"){
             return false
@@ -80,7 +80,7 @@ export default function NewBooking() {
                                         <DropDown title="Startzeit" items={bookingTimes} />
                                     </FormItem>
                                     <FormItem width="1/4" title="Zeit bis" icon={ClockIcon}>
-                                        <DropDown title="Endzeit" items={bookingTimes}/>
+                                        <DropDown title="Endzeit" items={bookingTimes} />
                                     </FormItem>
                                     <FormItem width="1/4">
                                         <button className="button-primary next-button" onClick={() => setCurrentStep(currentStep + 1)} >Jetzt suchen &rarr;</button>
@@ -88,48 +88,59 @@ export default function NewBooking() {
                                 </FormSection>
                             </FormContainer>
 
-                            }
-                            {
-                                currentStep == 2 &&
-                                <FormContainer title="Arbeitsplatztyp wählen">
-                                    <FormSection>
-                                        <FormItem width="1/2">
-                                            <button className={"button-select " + (workingPlaceType == 1 ? "background-green" : "bg-gray-100 hover:bg-gray-200")} onClick={() => handleSetWorkingPlaceType(1)}>Einzelarbeitsplatz</button>
-                                        </FormItem>
-                                        <FormItem width="1/2">
-                                            <button disabled={validateWorkPlaceType()} className={"button-select " + (workingPlaceType == 2 ? "background-green" : "bg-gray-100 hover:bg-gray-200")} onClick={() => handleSetWorkingPlaceType(2)}>Doppelarbeitsplatz</button>
-                                        </FormItem>
-                                    </FormSection>
-                                    <FormContainerEnd>
-                                        {workingPlaceType > 0 ? <button className="button-primary next-button" onClick={() => setCurrentStep(currentStep + 1)} >Weiter &rarr;</button> : ""}
-                                    </FormContainerEnd>
-                                </FormContainer>
+                        }
+                        {
+                            currentStep == 2 &&
+                            <FormContainer title="Arbeitsplatztyp wählen">
+                                <FormSection>
+                                    <FormItem width="1/2">
+                                        <button className={"button-select " + (workingPlaceType == 1 ? "background-green" : "bg-gray-100 hover:bg-gray-200")} onClick={() => handleSetWorkingPlaceType(1)}>Einzelarbeitsplatz</button>
+                                    </FormItem>
+                                    <FormItem width="1/2">
+                                        <button disabled={validateWorkPlaceType()} className={"button-select " + (workingPlaceType == 2 ? "background-green" : "bg-gray-100 hover:bg-gray-200")} onClick={() => handleSetWorkingPlaceType(2)}>Doppelarbeitsplatz</button>
+                                    </FormItem>
+                                </FormSection>
+                                <FormContainerEnd>
+                                    {workingPlaceType > 0 ? <button className="button-primary next-button" onClick={() => setCurrentStep(currentStep + 1)} >Weiter &rarr;</button> : ""}
+                                </FormContainerEnd>
+                            </FormContainer>
 
                             //3 Checkboxen für Browser
                             // 3 Checkboxen für Kommunikationsapplikationen
                         }
                         {
                             currentStep == 3 &&
-                            <FormContainer title="Arbeitsplätze konfigurieren">
-                                <FormSection title="Arbeitsplatz 1">
-                                    <FormItem title="Gerät wählen">
-                                        <RadioButtons items={geraete} />
-                                    </FormItem>
-                                </FormSection>
-                                <FormSection>
-                                    <FormItem title="Betriebssystem">
-                                        <RadioButtons items={betriebssysteme} />
-                                    </FormItem>
-                                </FormSection>
-                                <FormSection>
-                                    <FormItem title="Browser" width="1/2">
-                                        <CheckboxGroup items={browser} />
-                                    </FormItem>
-                                    <FormItem title="Kommunikationsapplikationen" width="1/2">
-                                        <CheckboxGroup items={kommunikationsapplikationen} />
-                                    </FormItem>
-                                </FormSection>
-                            </FormContainer>
+                            <div className="flex">
+                                <div className="w-9/12">
+                                    <FormContainer title="Arbeitsplätze konfigurieren">
+                                        <FormSection title="Arbeitsplatz 1">
+                                            <FormItem title="Gerät wählen">
+                                                <RadioButtons items={geraete} />
+                                            </FormItem>
+                                        </FormSection>
+                                        <FormSection>
+                                            <FormItem title="Betriebssystem">
+                                                <RadioButtons items={betriebssysteme} />
+                                            </FormItem>
+                                        </FormSection>
+                                        <FormSection>
+                                            <FormItem title="Browser" width="1/2">
+                                                <CheckboxGroup items={browser} />
+                                            </FormItem>
+                                            <FormItem title="Kommunikationsapplikationen" width="1/2">
+                                                <CheckboxGroup items={kommunikationsapplikationen} />
+                                            </FormItem>
+                                        </FormSection>
+                                    </FormContainer>
+                                </div>
+                                <div className="w-3/12 ml-5">
+                                    <div className="shadow-md p-5">
+                                        <p className="text-lg">Zusammenfassung</p>
+                                        <button className="button-primary w-full" onClick={() => setCurrentStep(currentStep + 1)} >Weiter &rarr;</button>
+
+                                    </div>
+                                </div>
+                            </div>
                         }
                         {
                             workingPlaceType == 2 && currentStep == 3 &&
@@ -154,21 +165,23 @@ export default function NewBooking() {
                         }
                         {currentStep == 3 &&
                             <div className="py-3 flex justify-end">
-                                <button className="button-primary next-button" onClick={() => setCurrentStep(currentStep + 1)} >Weiter &rarr;</button>
                             </div>
                         }
                         {
                             currentStep == 4 &&
                             <FormContainer title="Übersicht">
-                                <FormSection title="Zeitraum">
-                                    <div className="flex w-full item-center">
-                                        <div>
-                                            <p className="font-base">Zeitraum</p>
-                                            <p className="font-base">Von</p>
-                                            <p className="font-base">Bis</p>
-
-                                        </div>
-                                    </div>
+                                <FormSection>
+                                    <FormItem width="1/2">
+                                        <p className=""></p>
+                                        <p className="font-base">Zeitraum</p>
+                                        <p className="font-base">Von</p>
+                                        <p className="font-base">Bis</p>
+                                    </FormItem>
+                                    <FormItem width="1/2">
+                                        <p className="font-base">Zeitraum</p>
+                                        <p className="font-base">Von</p>
+                                        <p className="font-base">Bis</p>
+                                    </FormItem>
                                 </FormSection>
                                 <FormContainerEnd>
                                     <button className="button-primary next-button" onClick={() => setCurrentStep(currentStep + 1)} >Weiter &rarr;</button>
