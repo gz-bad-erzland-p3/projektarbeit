aa4cc1ef8254d5e00ee5f7c770f00dfc77993009
Richard Peters <rpe@xima.de>
2023-01-28 15:22:48 +0100

    beautify impressum

--- pages/impressum.tsx
+++ pages/impressum.tsx
@@ -8,7 +8,7 @@ export default function SupportMainPage() {
           <div className="flex justify-center mx-auto max-w-screen-xl mb-10">
             <div className={"bg-white px-5 rounded-2xl"}>
               <h1 className="text-3xl">Impressum</h1>
-              <h2>Gesetzliche Anbieterkennung:</h2>
+              <h2>Gesetzliche Anbieterkennung</h2><br />
               <p>
                 <b>Herausgeber:</b><br />
 
@@ -40,14 +40,14 @@ export default function SupportMainPage() {
                 Dr.-Külz-Ring 19<br />
                 01067 Dresden<br />
                 Telefon: +49 157 70601204<br />
-                Email: wir haben keine<br />
+                Email: wir haben keine<br /><br />
               </p><p>
                 <b>Haftungsausschluss</b><br />
                 Die Autoren übernehmen keinerlei Gewähr für die Aktualität, Korrektheit, Vollständigkeit oder Qualität der bereitgestellten Informationen. Haftungsansprüche gegen die Autoren, die sich auf Schäden materieller oder ideeller Art beziehen, welche durch die Nutzung oder Nichtnutzung der dargebotenen Informationen bzw. durch die Nutzung fehlerhafter und unvollständiger Informationen verursacht wurden sind grundsätzlich ausgeschlossen, sofern seitens der Autoren kein nachweislich vorsätzliches oder grob fahrlässiges Verschulden vorliegt.
-              </p><p>
+              </p><br /><p>
                 <b>Verweise und Links</b><br />
                 Bei direkten oder indirekten Verweisen auf fremde Internetseiten (“Links”), die außerhalb des Verantwortungsbereiches des Autors liegen, tritt eine Haftungsverpflichtung ausschließlich in dem Fall in Kraft, in dem der Autor von den Inhalten Kenntnis hat, er sich von ihnen nicht ausdrücklich distanziert und es ihm technisch möglich und zumutbar wäre, die Nutzung im Falle rechtswidriger Inhalte zu verhindern. Der Autor erklärt daher ausdrücklich, dass zum Zeitpunkt der Linksetzung die entsprechenden verlinkten Seiten frei von illegalen Inhalten waren. Der Autor hat keinerlei Einfluss auf die aktuelle und zukünftige Gestaltung und auf die Inhalte der gelinkten/verknüpften Seiten. Deshalb distanziert er sich hiermit ausdrücklich von allen Inhalten aller gelinkten /verknüpften Seiten und machen sich diese unter keinen Umständen zu Eigen. Diese Feststellung gilt für alle innerhalb des eigenen Internetangebotes gesetzten Links und Verweise sowie für Fremdeinträge in vom Autor eingerichteten Gästebüchern, Diskussionsforen und Mailinglisten. Für illegale, fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder Nichtnutzung solcherart dargebotener Informationen entstehen, haftet allein der Anbieter der Seite, auf welche verwiesen wurde, nicht derjenige, der über Links auf die jeweilige Veröffentlichung lediglich verweist.
-              </p><p>
+              </p><br /><p>
                 <b>Urheberrecht</b><br />
                 Das Copyright für veröffentlichte, von Autoren selbst erstellte Objekte bleibt allein bei den Autoren der Seiten. Eine Vervielfältigung oder Verwendung solcher Grafiken, Tondokumente, Videosequenzen und Texte in anderen elektronischen oder gedruckten Publikationen ist ohne ausdrückliche Zustimmung der Autoren nicht gestattet.
               </p>
